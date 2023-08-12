9770694fdacc97dee80abb514e4de7cdccfe7044
Vijay <Vijay.rathnappa42@gmail.com>
2023-08-12 11:05:02 +0530

    Added Skills, Portfolio and Footer Section.

---
+++ script.js
@@ -0,0 +1,12 @@
+// To turn navbar dark when scrolling through the website
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
